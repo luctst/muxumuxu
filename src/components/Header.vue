50395a2fe2ce393ad5058402bd19b13f73f27a8b
luctst <lucas.tostee@gmail.com>
2021-01-14 12:34:20 +0100

    Add logic for chrono

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,28 @@
 <template>
-    <header>
-        <p>{{ mn }}mn-{{ seconds }}s</p>
+    <header class="container mt-3">
+        <div class="row">
+            <div class="col-12 d-flex justify-content-between align-items-center">
+                <h1 class="h2">Muxu game</h1>
+                <p v-if="displayChrono" :class="handleClass" class="text-light rounded h4">{{ mn }}mn-{{ seconds }}s</p>
+                <p v-if="mn === 0 && seconds === 0" class="text-muted">Timeout c'est dead wallah</p>
+            </div>
+        </div>
     </header>
 </template>
 
 <script>
 export default {
+    name: 'header-vue',
     data() {
         return {
-            mn: 2,
+            mn: 3,
             seconds: 10
+        }
+    },
+    props: {
+        gameStarted: {
+            type: Boolean,
+            required: true
         }
     },
     created() {
@@ -32,16 +45,44 @@ export default {
 
         }.bind(this), 1000);
     },
+    computed: {
+        handleClass() {
+            if (this.mn >= 2) {
+                return 'bg-success';
+            }
+
+            if (this.mn >= 1) {
+                return 'bg-warning';
+            }
+
+            return 'bg-danger';
+        },
+        displayChrono()  {
+            if (!this.gameStarted) return false;
+
+            if (this.mn === 0 && this.seconds === 0) return false;
+            return true;
+        }
+    },
 }
 </script>
 
 <style scoped>
 header {
     display: flex;
-    justify-content: flex-end;
+    justify-content: space-between;
 }
 
 header p {
     margin: 0;
+    padding: 1%;
+}
+
+.row {
+    width: inherit;
+}
+
+.col-12 {
+    width: inherit;
 }
 </style>
