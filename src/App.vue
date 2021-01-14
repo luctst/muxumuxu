bb98790ee61d7ddda8d0643e58635a67c7eeb717
luctst <lucas.tostee@gmail.com>
2021-01-14 12:33:46 +0100

    Add logic to start a game

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,34 @@
 <template>
   <div id="app">
-    <header-vue></header-vue>
+    <header-vue :gameStarted="gameStarted"></header-vue>
+    <main class="container">
+      <words 
+        :gameStarted="gameStarted"
+        @gameStatus="gameStatus">
+      </words>
+    </main>
   </div>
 </template>
 
 <script>
 import Header from './components/Header';
+import Words from './components/Words';
 
 export default {
   name: 'App',
   components: {
-    'header-vue': Header 
+    'header-vue': Header,
+    Words
+  },
+  data() {
+    return {
+      gameStarted: false
+    }
+  },
+  methods: {
+    gameStatus(value) {
+      this.gameStarted = value;
+    }
   }
 }
 </script>
@@ -22,5 +40,9 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+main {
+  height: 80vh;
 }
 </style>
