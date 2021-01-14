e388e44dd3bfb5af5e4e9884133bb97eb2203f38
luctst <lucas.tostee@gmail.com>
2021-01-14 12:34:04 +0100

    Add vue-js-modal config

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VModal from 'vue-js-modal';
 
 Vue.config.productionTip = false
+
+Vue.use(VModal, { dynamicDefault: { draggable: false, resizable: false }});
 
 new Vue({
   render: h => h(App),
